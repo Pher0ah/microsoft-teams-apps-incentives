39d5713bcbdbb1ee997159c76311726cf932d6c0
Hany <pheroah@gmail.com>
2020-07-17 12:28:47 +1000

    Update Create-Incentive-SharePointList.ps1
    Fixed issues with connecting using MFA, PowerShell v.7 and the errors from adding fields that already exist.

--- Deployment/Create-Incentive-SharePointList.ps1
+++ Deployment/Create-Incentive-SharePointList.ps1
@@ -1,37 +1,52 @@
-﻿param([string]$siteurl,[string]$baseurl,[string]$sitename)
+﻿#Requires -Version 5.0
+#Script Parameters
+[cmdletbinding(SupportsShouldProcess=$True)]Param(
+  [Parameter(Mandatory=$True, ValueFromPipeline=$true, ParameterSetName="currentSite")][string]$siteURL,
+  [Parameter(Mandatory=$True, ValueFromPipeline=$true, ParameterSetName="newSite"    )][string]$baseURL,
+  [Parameter(Mandatory=$True, ValueFromPipeline=$false, ParameterSetName="newSite"    )][string]$siteName
+)
+
+#Check Which Version of PowerShell is being used
+if($host.version -ge "7.0.0"){
+  $loadModuleCmd = "import-module -Name SharePointPnPPowerShellOnline -UseWindowsPowerShell"
+}else{
+  Write-Warning "Using PowerShell version: $($host.version); it is recommended to upgrade to 7.0.0 or above"
+  $loadModuleCmd = "import-module -Name SharePointPnPPowerShellOnline"
+}
 
 #Making sure the user has this Package installed.
-Install-Module SharePointPnPPowerShellOnline
-
-#Getting credentials of user to login into given sharepoint site
-$usercredential = Get-Credential
-
-#When user enters only 1 param and that param is $siteurl then it adds the list inside that site url
-if(($psboundparameters.Count -eq 1) -and $siteurl ){
-
-#Connects to the sharepoint site URL
-Connect-PnPOnline -Url $siteurl -Credentials $usercredential
+if(-not (Get-Module -ListAvailable -Name "SharePointPnPPowerShellOnline")){
+  Install-Module SharePointPnPPowerShellOnline -Scope User
 }
 
-#when user enters 2 params and those 2 are baseurl and sitename then it will create the SharePoint site using SP base URL
-if(($psboundparameters.Count -eq 2) -and ($baseurl) -and $sitename){
+#Load the SharePointOnline Module
+Invoke-Expression -command $loadModuleCmd
 
-#Connects to the sharepoint base URL
-Connect-PnPOnline -Url $baseurl -Credentials $usercredential
+#When user enters $siteURL then it adds the list inside that site url
+if($siteURL){
+  $incentivesiteURL = $siteURL
+}
 
-#Creates the Data Store Incentives App 
-#Title and alias for new site needs to be unique
-$incentivesiteurl = New-PnPSite -Type TeamSite -Title $sitename -Alias $sitename
+#when user enters $baseURL and $siteName then it will create a new SharePoint site for the list
+if($baseURL){
+  #Connects to the sharepoint base URL
+  Connect-PnPOnline -Url $baseURL -UseWebLogin
+
+  #Creates the Data Store Incentives App 
+  #Title and alias for new site needs to be unique
+  $incentivesiteURL = New-PnPSite -Type TeamSite -Title $siteName -Alias $siteName
+}
 
 #Switchs to the newly created site
-Connect-PnPOnline -Url $incentivesiteurl -Credentials $usercredential
-}
+Connect-PnPOnline -Url $incentivesiteURL -UseWebLogin
 
-#Creates a new list for Incentives
+<#####################################################################################
+Creates a new list for Incentives
+#####################################################################################>
 New-PnPList -Title 'Incentives' -Template GenericList -Url Lists/Incentives -ErrorAction Continue
 
 #Adds new fields(columns) for Incentives
-Add-PnPField -List "Incentives" -DisplayName "Title" -InternalName "Title" -Type Text -AddToDefaultView
+#Add-PnPField -List "Incentives" -DisplayName "Title" -InternalName "Title" -Type Text -AddToDefaultView #This is not required as it is created automatically
 Add-PnPField -List "Incentives" -DisplayName "Name" -InternalName "Name" -Type Text -AddToDefaultView
 Add-PnPField -List "Incentives" -DisplayName "Points" -InternalName "Points" -Type Number -AddToDefaultView
 Add-PnPField -List "Incentives" -DisplayName "IncentiveCode" -InternalName "IncentiveCode" -Type Text -AddToDefaultView
@@ -41,18 +56,23 @@ Add-PnPField -List "Incentives" -DisplayName "DueDate" -InternalName "DueDate" -
 Add-PnPField -List "Incentives" -DisplayName "UpdatedBy" -InternalName "UpdatedBy" -Type Text -AddToDefaultView
 Add-PnPField -List "Incentives" -DisplayName "UpdatedOn" -InternalName "UpdatedOn" -Type DateTime -AddToDefaultView
 
-#Attachments column is present by default in sharepoint lists, this may show error like "Attachments field exists"
-#Need to click on add column inside list and goto Show/hide columns and make Attachments visible
-Add-PnPField -List "Incentives" -DisplayName "Attachments" -InternalName "Attachments" -Type Attachments -AddToDefaultView
+#Attachments column is present by default in sharepoint lists, but needs to be visible
+$incentivesFields = (Get-PnPView -List "Incentives").ViewFields
+If($incentivesFields -notcontains "Attachments"){
+  $incentivesFields += "Attachments"
+  Set-PnPView -List "Incentives" -Fields $incentivesFields -Identity (Get-PnPView -List "Incentives").Id
+}
+
 Add-PnPField -List "Incentives" -DisplayName "IsIncentiveLive" -InternalName "IsIncentiveLive" -Type Boolean -AddToDefaultView
 
 
-
-#Creates a new list for Rewards
+<#####################################################################################
+Creates a new list for Rewards
+#####################################################################################>
 New-PnPList -Title 'Rewards' -Template GenericList -Url Lists/Rewards -ErrorAction Continue
 
 #Adds new fields for Rewards
-Add-PnPField -List "Rewards" -DisplayName "Title" -InternalName "Title" -Type Text -AddToDefaultView
+#Add-PnPField -List "Rewards" -DisplayName "Title" -InternalName "Title" -Type Text -AddToDefaultView #This is not required as it is created automatically
 Add-PnPField -List "Rewards" -DisplayName "Name" -InternalName "Name" -Type Text -AddToDefaultView
 Add-PnPField -List "Rewards" -DisplayName "Points" -InternalName "Points" -Type Number -AddToDefaultView
 Add-PnPField -List "Rewards" -DisplayName "CreatedBy" -InternalName "CreatedBy" -Type Text -AddToDefaultView
@@ -65,12 +85,13 @@ Add-PnPField -List "Rewards" -DisplayName "UpdatedOn" -InternalName "UpdatedOn" 
 Add-PnPField -List "Rewards" -DisplayName "Attachments" -InternalName "Attachments" -Type Attachments -AddToDefaultView
 
 
-#Creates a new list for UserIncentives
+<#####################################################################################
+Creates a new list for UserIncentives
+#####################################################################################>
 New-PnPList -Title 'UserIncentives' -Template GenericList -Url Lists/UserIncentives -ErrorAction Continue
 
 #Adds new fields for UserIncentives
-
-Add-PnPField -List "UserIncentives" -DisplayName "Title" -InternalName "Title" -Type Text -AddToDefaultView
+#Add-PnPField -List "UserIncentives" -DisplayName "Title" -InternalName "Title" -Type Text -AddToDefaultView #This is not required as it is created automatically
 Add-PnPField -List "UserIncentives" -DisplayName "UserName" -InternalName "UserName" -Type Text -AddToDefaultView
 Add-PnPField -List "UserIncentives" -DisplayName "IncentiveName" -InternalName "IncentiveName" -Type Text -AddToDefaultView
 Add-PnPField -List "UserIncentives" -DisplayName "IncentivePoints" -InternalName "IncentivePoints" -Type Number -AddToDefaultView
@@ -79,11 +100,13 @@ Add-PnPField -List "UserIncentives" -DisplayName "ReceivedOn" -InternalName "Rec
 Add-PnPField -List "UserIncentives" -DisplayName "ReasonForEdit" -InternalName "ReasonForEdit" -Type Text -AddToDefaultView
 
 
-#Creates a new list for UserRewards
+<#####################################################################################
+Creates a new list for UserRewards
+#####################################################################################>
 New-PnPList -Title 'UserRewards' -Template GenericList -Url Lists/UserRewards -ErrorAction Continue
 
 #Adds New Fields for UserRewards
-Add-PnPField -List "UserRewards" -DisplayName "Title" -InternalName "Title" -Type Text -AddToDefaultView
+#Add-PnPField -List "UserRewards" -DisplayName "Title" -InternalName "Title" -Type Text -AddToDefaultView #This is not required as it is created automatically
 Add-PnPField -List "UserRewards" -DisplayName "UserName" -InternalName "UserName" -Type Text -AddToDefaultView
 Add-PnPField -List "UserRewards" -DisplayName "RewardName" -InternalName "RewardName" -Type Text -AddToDefaultView
 Add-PnPField -List "UserRewards" -DisplayName "RewardPoint" -InternalName "RewardPoint" -Type Number -AddToDefaultView
